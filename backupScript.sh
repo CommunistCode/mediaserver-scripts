8e73fcc54a512e14b2b98a272d373be58228f4db
Jack Mitchell <jack@communistcode.co.uk>
2012-03-03 18:19:11 +0000

    add mysql database backup

--- backupScript.sh
+++ backupScript.sh
@@ -1,10 +1,34 @@
 #! /bin/bash
 
-body=$(rdiff-backup --print-statistics jack@embed.me.uk::/srv/http /mnt/seaRaid/Backup/embed.me.uk/http)
+# Get the passwords required
+source secretPasswords
+
+rand=$RANDOM
+tmpBackupFile="/tmp/backup-${rand}"
+
+echo -e 'HTTP Backup\n' > ${tmpBackupFile}
+
+# Backup http
+rdiff-backup --print-statistics jack@embed.me.uk::/srv/http /mnt/seaRaid/Backup/embed.me.uk/http >> ${tmpBackupFile}
+
+echo -e '\nvMail Backup\n' >> ${tmpBackupFile}
+
+# Backup vmail
+rdiff-backup --print-statistics root@embed.me.uk::/var/vmail /mnt/seaRaid/Backup/embed.me.uk/vmail >> ${tmpBackupFile}
+
+echo -e '\nmySQL Backup\n' >> ${tmpBackupFile}
+
+# Backup mySQL Databases
+if ssh -l jack embed.me.uk mysqldump -u root -p${mysqlPassword} --all-databases > /mnt/seaRaid/Backup/embed.me.uk/database/full-dump-$(date +%s).sql;
+	then echo "Success!" >> ${tmpBackupFile}
+else
+	echo "Failure!" >> ${tmpBackupFile}
+fi
 
 to="jack@embed.me.uk"
 from="mediaserver@home"
 subject="Backup Report: embed.me.uk ($(date))"
 
-/home/jack/sendEmail.sh "${to}" "${from}" "${subject}" "${body}"
+/home/jack/scripts/sendEmail.sh "${to}" "${from}" "${subject}" "$(cat ${tmpBackupFile})"
 
+rm ${tmpBackupFile}
