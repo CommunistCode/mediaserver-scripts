aa434d49f3753aa9a5065709018366ac804f3cb0
Jack Mitchell <jack@communistcode.co.uk>
2012-03-03 14:26:21 +0000

    improved check raid array script

--- checkRAIDArray.sh
+++ checkRAIDArray.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
+to="jack@embed.me.uk"
+from="mediaserver@home"
+subject="ALERT: Raid Degredation Detected ($(date))"
+
 RAIDStatus=$(cat /proc/mdstat)
 
 if [[ "$RAIDStatus" =~ "[UUUUU]" ]]
 then
-	echo "matched"
+	# Do nothing
+	echo 'All good in the hood!'
 else
-	echo "not matched"
+	/home/jack/scripts/sendEmail.sh "${to}" "${from}" "${subject}" "${RAIDStatus}"
+	echo 'Uh oh - stuffs going down!'
 fi
