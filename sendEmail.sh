024e0601974753953aec9acacb833d491afe0f0e
Jack Mitchell <jack@communistcode.co.uk>
2012-03-09 13:51:58 +0000

    improved sendMail script to include valid date

--- sendEmail.sh
+++ sendEmail.sh
@@ -10,6 +10,7 @@ rand=$RANDOM
 echo "To: ${to}" > /tmp/email-${rand}
 echo "From: ${from}" >> /tmp/email-${rand}
 echo "Subject: ${subject}" >> /tmp/email-${rand}
+echo "Date: $(date --rfc-822)" >> /tmp/email-${rand}
 echo "${body}" >> /tmp/email-${rand}
 
 cat /tmp/email-${rand} | msmtp -a default ${to}
